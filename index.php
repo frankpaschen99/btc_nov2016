235287d5fa95fee0ee5d36b8065c3be812b43935
Pedro Safi <jpsafi@gmail.com>
2016-10-29 13:43:51 -0400

    fuck this

--- index.php
+++ index.php
@@ -123,7 +123,7 @@ require_once("functions.php");
 				</div>
 			</div>
 			<div class="row animate-box">
-				<div class="col-md-12">
+				<div class="col-md-12" style="text-align:center">
 					<?php
 						if (isset($_GET["depadd"]) && preg_match("^[13][a-km-zA-HJ-NP-Z1-9]{25,34}$^", $_GET["depadd"])) {
 							echo("<div class='col-md-8 col-md-offset-2 text-center gtco-heading'><img src=https://chart.googleapis.com/chart?cht=qr&chs=192x192&chl=" . $_GET["depadd"] . "/></div>");
